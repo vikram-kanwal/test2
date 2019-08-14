88514eaabfb36f42f72cfbe8e98e48adad73b603
vikram-kanwal <vikramkanwal01@gmail.com>
2019-08-13 21:31:19 -0500

    second java program

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Hwllowrold {
+
+     public static void main(String[]  args)  {
+         // Prints "Hello.world" to be terminal window.
+         System.out.println("Hello. World"):
+   }
+}
